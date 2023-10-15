160038313ae137c666e33a18cac6245fa363651e
Paweł Załęski <p.zaleski@samsung.com>
2023-10-15 16:53:07 +0200

    Loop mixed with jsx

--- react/react001/src/components/footer/Footer.js
+++ react/react001/src/components/footer/Footer.js
@@ -2,13 +2,27 @@ import React from "react";
 import './footer.css';
 import Copyright from '../copyright/Copyright';
 
+ 
+
 function Footer(props) {
+  const elements1 = ["TOS", "about"];
+  const items = [];
+  
+
+  for( const[index, value] of elements1.entries()) {
+
+    items.push(<ul><li>{value}</li></ul>);
+
+  } 
   return(
     <div>
+      
       <ul>
+        {items}
         <li>Regulamin</li>
         <li>FAQ</li>
         <li>Kontakt: {props.contact.adress}, {props.companyData.email}</li>
+        
       </ul>
       <Copyright year="2023" />
     </div>
