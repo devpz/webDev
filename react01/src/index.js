d9b59464b9cbd93836d689d515e72750a1e6e1cd
Paweł Załęski <p.zaleski@samsung.com>
2023-10-16 15:35:33 +0200

    Adding react01

---
+++ react01/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// function Greeting() {
+//   return <h2>My First Component</h2>;
+// }
+
+function Greeting() {
+  return React.createElement("h2", {}, "Hello world!");
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
