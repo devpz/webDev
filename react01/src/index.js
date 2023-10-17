82df4a58aa4600b7b5b7c923723bb9f627dce68e
Paweł Załęski <p.zaleski@samsung.com>
2023-10-17 16:52:42 +0200

    updated 002 react 01

--- react01/src/index.js
+++ react01/src/index.js
@@ -11,13 +11,21 @@ const BookList = () => {
         title={firstBook.title}
         job="developer"
         img={firstBook.img}
-      />
+        >
+          <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
+        </p>
+        <button>Click me</button> 
+              </Book>
+      
       <Book
         author={secondBook.author}
         title={secondBook.title}
         number={22}
         img={secondBook.img}
       />
+      
     </section>
   );
 };
@@ -34,11 +42,14 @@ const secondBook = {
   img: "https://images-na.ssl-images-amazon.com/images/I/91ke43dIxkL._AC_UL900_SR900,600_.jpg",
 };
 
-const Book = (props, firstBook) => {
+const Book1 = ({ img, title, author, children }) => {
+  // rest of logic
+};
+
+const Book = (props) => {
   // const Book = ({ img, title, author }) => {
-  console.log(firstBook);
   console.log(props);
-  const { img, title, author, job, number } = props;
+  const { img, title, author, job, number, children} = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
@@ -48,6 +59,7 @@ const Book = (props, firstBook) => {
       <p>{job}</p>
       <p>{title}</p>
       <p>{number}</p>
+      {children}
     </article>
   );
 };
